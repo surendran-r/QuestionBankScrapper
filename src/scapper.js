90a99cea41584539941eae9d45cf286a742a0856
Rangasamy <sura@amazon.com>
2015-08-30 01:16:01 +0530

    Modularised and added debug utils

--- src/scapper.js
+++ src/scapper.js
@@ -12,45 +12,64 @@ function gotoNextPage(i) {
 	}
 }
 
+function injectDebugFunctions(page) {
+	page.onConsoleMessage = function(msg, lineNum, sourceId) {
+	  console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
+	};
+
+	page.onResourceRequested = function (req) {
+	    console.log('requested: ' + req.url);
+	};
+
+	page.onResourceReceived = function (res) {
+	    console.log('received: ' + req.url);
+};
+}
+
 function getQandAFromURL(url, i) {
 	var page = webpage.create();
-	page.open(url, function (status) {
-			console.log('Loaded Page ... ');
-	        if (status !== 'success') {
-	            console.log('Unable to load the address! ' + status);
-	            phantom.exit(1);
-	        } else {
-	        	page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
-					console.log('Loaded JQuery ... ');
-					var data = page.evaluate( function() {
-						var results = new Array();
-			            //Get the question & answer
-						$(".bix-tbl-container").each(function (i, elem) {
-							var result = { question: "", option:[], answer:""};
-							//Get the question
-							console.log("Q:" + $(elem).find('.bix-td-qtxt').text());
-							result.question = $(elem).find('.bix-td-qtxt').text();
-							//Get the options
-							$(elem).find('.bix-td-option').each(function  (i, elem) {
-								var optionText = $(elem).text();
-								if(optionText === 'A.' || optionText === 'B.' || optionText === 'C.' || optionText === 'D.') {
-									return;
-								}
-								result.option.push(optionText);
-								console.log("O: " + optionText);
-							});
-							//Get the answer
-							console.log("A: " + $(elem).find('.jq-hdnakqb').html());
-							result.answer = $(elem).find('.jq-hdnakqb').html();
-							results.push(result);
+
+	function handler(status) {
+		console.log('Loaded Page ... ');
+	    if (status !== 'success') {
+	        console.log('Unable to load the address! ' + status);
+	        phantom.exit(1);
+	    } else {
+	    	function extractor() {
+				console.log('Loaded JQuery ... ');
+				var data = page.evaluate( function() {
+					var results = new Array();
+		            //Get the question & answer
+					$(".bix-tbl-container").each(function (i, elem) {
+						var result = { question: "", option:[], answer:""};
+						//Get the question
+						console.log("Q:" + $(elem).find('.bix-td-qtxt').text());
+						result.question = $(elem).find('.bix-td-qtxt').text();
+						//Get the options
+						$(elem).find('.bix-td-option').each(function  (i, elem) {
+							var optionText = $(elem).text();
+							if(optionText === 'A.' || optionText === 'B.' || optionText === 'C.' || optionText === 'D.') {
+								return;
+							}
+							result.option.push(optionText);
+							console.log("O: " + optionText);
 						});
-						return results;
+						//Get the answer
+						console.log("A: " + $(elem).find('.jq-hdnakqb').html());
+						result.answer = $(elem).find('.jq-hdnakqb').html();
+						results.push(result);
 					});
-					console.log(JSON.stringify(data));
-		            gotoNextPage(i);
-	        	});
+					return results;
+				});
+				console.log(JSON.stringify(data));
+	            gotoNextPage(i);
 	    	}
-	    });
+	    	page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", extractor);
+		}
+	}
+
+	injectDebugFunctions(page);
+	page.open(url, handler);
 }
 
 getQandAFromURL(urlPrefix, 1);
